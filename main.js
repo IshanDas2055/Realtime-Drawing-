9cab3b10f8493efa00fb70f7f33bfa15d955b52c
IshanDas2055 <95131977+IshanDas2055@users.noreply.github.com>
2021-12-05 19:59:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,24 +2,39 @@ function setup() {
 video = createCapture(VIDEO);
 video.size(550,500);
 
-canvas= createCanvas(550,550);
+canvas = createCanvas(550,550);
 canvas.position(560,150);
 
 poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose',gotPoses);
+poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     background('#00ff91');
+    fill('#66ff66');
+    stroke("#ff6666");
+    square(noseX, noseY, difference);
 }
 
-function modelLoaded() {
-    console.log('PoseNet Is Intialized !');
+function modelLoaded(){
+    console.log('POSENET HAS BEEN INTILIALIZED');
 }
-
+noseX=0;
+noseY=0;
+difference = 0;
+rightWrist = 0;
+leftWrist = 0;
 function gotPoses(results){
-    if(results.length > 0)
-    {
-        console.log(results);
-    }
+if(results.length > 0)
+{
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +"noseY = " + noseY )
+    rightWrist = results[0].pose.rightWrist.x;
+    leftWrist = results[0].pose.leftWrist.x;
+    difference = floor(leftWrist - rightWrist);
+
+    console.log("leftWrist = " + leftWrist + "rightWrist = " + rightWrist + "difference" + difference);
+}
 }
